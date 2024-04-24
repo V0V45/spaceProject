f43322d337622fb30cd1c4f007e7e0c6b0408887
V0V45 <vovanfilatov@yandex.ru>
2024-04-24 22:35:46 +0300

    Added about us field

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 // Переменные и константы
 const viewPortfolioButton = document.getElementsByClassName('statistics__viewPortfolioButton')[0];
 const viewPortofolioArrow = document.getElementsByClassName('statistics__viewPortfolioButton__ArrowIcon')[0];
+const aboutUsButton = document.getElementsByClassName('aboutUs__infoRight__button')[0];
+const aboutUsArrow = document.getElementsByClassName('aboutUs__infoRight__button__ArrowIcon')[0];
 
 // Функции
 function goToPortfolio() {
+    window.location.href = 'portfolio.html';
+}
+
+function goToAboutUs() {
     window.location.href = 'about.html';
 }
 
-function changeArrowOnHoverToStraight() {
-    viewPortofolioArrow.src = './src/icons/arrow_right.png';
+function changePortfolioArrowOnHoverToStraight() {
+    viewPortofolioArrow.src = './src/icons/arrow_right.svg';
 }
 
-function changeArrowOnHoverToUp() {
-    viewPortofolioArrow.src = './src/icons/arrow_up.png';
+function changePortfolioArrowOnHoverToUp() {
+    viewPortofolioArrow.src = './src/icons/arrow_up.svg';
+}
+
+function changeAboutUsArrowOnHoverToStraight() {
+    aboutUsArrow.src = './src/icons/arrow_right.svg';
+}
+
+function changeAboutUsArrowOnHoverToUp() {
+    aboutUsArrow.src = './src/icons/arrow_up.svg';
 }
 
 // Блок выполнения
-viewPortfolioButton.addEventListener('mouseenter', changeArrowOnHoverToStraight);
-viewPortfolioButton.addEventListener('mouseleave', changeArrowOnHoverToUp);
+viewPortfolioButton.addEventListener('mouseenter', changePortfolioArrowOnHoverToStraight);
+viewPortfolioButton.addEventListener('mouseleave', changePortfolioArrowOnHoverToUp);
+aboutUsButton.addEventListener('mouseenter', changeAboutUsArrowOnHoverToStraight);
+aboutUsButton.addEventListener('mouseleave', changeAboutUsArrowOnHoverToUp);
 
 
 
