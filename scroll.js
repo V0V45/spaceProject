c6f8764a6ad4cd5086eceeed908cf4bb805f87d1
V0V45 <vovanfilatov@yandex.ru>
2024-05-19 17:13:45 +0300

    Added animation to 8 cards

--- scroll.js
+++ scroll.js
@@ -1,5 +1,5 @@
 // Скрипт анимаций при скроллинге
-// для появляющихся снизу вверх
+// 1) Появление снизу вверх
 // для каждого входного элемента если элемент пересекает viewport добавляем ему класс show, и если не пересекает то удаляем
 const observerFromDown = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -9,9 +9,11 @@ const observerFromDown = new IntersectionObserver((entries) => {
             // entry.target.classList.remove('showFromDown'); // при пересечении второй раз будет появляться еще раз - пока убрал
         }
     })
-})
+});
+const hiddenFromDownElements = document.querySelectorAll('.hiddenFromDown'); // все элементы с классом hiddenFromDown в HTML записываем в массив
+hiddenFromDownElements.forEach((element) => observerFromDown.observe(element)); // для каждого из них запускаем наблюдение
 
-// для цифр
+// 2) Плавное появление цифр
 // для каждого входного элемента если элемент пересекает viewport запускаем отсчет от 0 до конечного значения
 const observerDigits = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -34,12 +36,7 @@ const observerDigits = new IntersectionObserver((entries) => {
             observerDigits.unobserve(entry.target); // убираем слежку за объектом
         }
     })
-})
-
-// для анимации снизу вверх:
-const hiddenFromDownElements = document.querySelectorAll('.hiddenFromDown'); // все элементы с классом hiddenFromDown в HTML записываем в массив
-hiddenFromDownElements.forEach((element) => observerFromDown.observe(element)); // для каждого из них запускаем наблюдение
-// для анимации чисел:
+});
 const rating = document.getElementsByClassName('aboutUs__digits__rating__span')[0]; // элемент рейтинга
 const satisfiedUsers = document.getElementsByClassName('aboutUs__digits__satisfiedUsers__span')[0]; // элемент удовлетворенных пользователей
 const experience = document.getElementsByClassName('aboutUs__digits__experience__span')[0]; // элемент опытных сотрудников
@@ -47,4 +44,27 @@ observerDigits.observe(rating); // наблюдаем за появлением 
 observerDigits.observe(satisfiedUsers); // наблюдаем за появлением элемента удовлетворенных пользователей на экране
 observerDigits.observe(experience); // наблюдаем за появлением элемента опытных сотрудников на экране
 
+// 3) Появление слева
+// для каждого входного элемента если элемент пересекает viewport добавляем ему класс show, и если не пересекает то удаляем
+const observerFromLeft = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showFromLeft');
+        }
+    })
+});
+const hiddenFromLeftElements = document.querySelectorAll('.hiddenFromLeft'); // все элементы с классом hiddenFromLeft в HTML записываем в массив
+hiddenFromLeftElements.forEach((element) => observerFromLeft.observe(element)); // для каждого из них запускаем наблюдение
+
+// 4) Появление справа
+// для каждого входного элемента если элемент пересекает viewport добавляем ему класс show, и если не пересекает то удаляем
+const observerFromRight = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showFromRight');
+        }
+    })
+});
+const hiddenFromRightElements = document.querySelectorAll('.hiddenFromRight'); // все элементы с классом hiddenFromRight в HTML записываем в массив
+hiddenFromRightElements.forEach((element) => observerFromRight.observe(element)); // для каждого из них запускаем наблюдение
 
