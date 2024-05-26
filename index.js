e200ae2a44c3ca49d8a156358279be355834f372
V0V45 <vovanfilatov@yandex.ru>
2024-05-26 04:19:14 +0300

    Added form sending logic

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ const imagesArray = [
     document.getElementsByClassName('expertise__slider__image3')[0],
     document.getElementsByClassName('expertise__slider__image4')[0],
 ];
+const backCallForm = document.getElementsByClassName('backCall__inputForm')[0];
+const popUpSection = document.getElementsByClassName('popUp')[0];
+const popUpContainer = document.getElementsByClassName('popUpContainer')[0];
+const popUpText = document.getElementsByClassName('popUp__text')[0];
 
 
 // Функции
@@ -161,6 +165,47 @@ function changeActiveImage(previousActiveSpoilerID, currentActiveSpoilerID) {
     }
 }
 
+// Работа отправки формы обратного звонка
+function submitBackCallForm(event) {
+    event.preventDefault(); // отменяем действие при отправке формы по умолчанию
+    let formData = new FormData(backCallForm); // создаем объект класса FormData, который черпает информацию из полей формы
+    let formDataToBePosted = Object.fromEntries(formData); // преобразуем вышеописанный объект в объект "ключ-значение"
+    // тем самым вышеописанная переменная хранит в себе все заполненные поля в форме
+    getResponseFromServerAboutBackcall(formDataToBePosted); // передаем поля формы на сервер
+}
+
+// Отправка формы на сервер
+async function getResponseFromServerAboutBackcall(formDataToBePosted) {
+    // обращаемся к backcall через POST и передаем туда JSON из заполненных в форме данных
+    let response = await fetch('/backcall', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formDataToBePosted),
+    });
+    // ответ от сервера также приходит в формате json
+    let result = await response.json();
+    // нам приходит объект, который содержит ключ email: sent или email: notSent
+    // если сообщение успешно обработано сервером, т.е. мы получили ключ sent
+    if (result.email === 'sent') {
+        // выдаем сообщение что все хорошо
+        popUpText.innerHTML = 'Thank you, your request has been sent, our specialists will contact you within 24 hours.';
+        // активируем popUp окно
+        popUpSection.classList.add('showPopUp');
+    } else { // в других случаях
+        // выдаем сообщение что что-то пошло не так
+        popUpText.innerHTML = 'Something went wrong. Please, try again later';
+        // активируем popUp окно
+        popUpSection.classList.add('showPopUp');
+    }
+}
+
+// Логика кнопки закрытия pop-up окна
+function closePopUp() {
+    popUpSection.classList.remove('showPopUp');
+}
+
 // Обработчики событий
 viewPortfolioButton.addEventListener('mouseenter', changePortfolioArrowOnHoverToStraight);
 viewPortfolioButton.addEventListener('mouseleave', changePortfolioArrowOnHoverToUp);
@@ -193,3 +238,4 @@ discussButton.addEventListener('mouseleave', changeWhiteArrowToUp);
 spoilerArray.forEach(element => {
     element.addEventListener('click', changeActiveSpoilerIndex);
 });
+backCallForm.addEventListener('submit', submitBackCallForm);
