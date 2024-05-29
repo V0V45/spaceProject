963b3fbb8c5aec9a42e86a8ff627b16411ae8b70
V0V45 <vovanfilatov@yandex.ru>
2024-05-30 01:30:32 +0300

    Added validation

--- index.js
+++ index.js
@@ -30,6 +30,18 @@ const popUpSection = document.getElementsByClassName('popUp')[0];
 const popUpContainer = document.getElementsByClassName('popUpContainer')[0];
 const popUpText = document.getElementsByClassName('popUp__text')[0];
 const contactUsForm = document.getElementsByClassName('contactUs__form')[0];
+const radioButtonsArray = [
+    document.getElementsByClassName('backCall__inputForm__communicationChoice__buttonsAndLabels__telegram-radioButton')[0],
+    document.getElementsByClassName('backCall__inputForm__communicationChoice__buttonsAndLabels__whatsApp-radioButton')[0],
+    document.getElementsByClassName('backCall__inputForm__communicationChoice__buttonsAndLabels__email-radioButton')[0],
+    document.getElementsByClassName('backCall__inputForm__communicationChoice__buttonsAndLabels__phone-radioButton')[0],
+];
+const nameInputField = document.getElementsByClassName('backCall__inputForm__nameInputField')[0];
+const phoneOrEmailInputField = document.getElementsByClassName('backCall__inputForm__phoneOrEmailInputField')[0];
+const firstNameInput = document.getElementsByClassName('contactUs__form__firstNameInput')[0];
+const eMailInput = document.getElementsByClassName('contactUs__form__eMailInput')[0];
+const scheduleCallButton = document.getElementsByClassName('backCall__inputForm__scheduleCall__button')[0];
+const freeConsultationButton = document.getElementsByClassName('contactUs__form__submitButton')[0];
 
 
 // Функции
@@ -170,10 +182,11 @@ function submitBackCallForm(event) {
     let formData = new FormData(backCallForm); // создаем объект класса FormData, который черпает информацию из полей формы
     let formDataToBePosted = Object.fromEntries(formData); // преобразуем вышеописанный объект в объект "ключ-значение"
     // тем самым вышеописанная переменная хранит в себе все заполненные поля в форме
-    sendFormToServer(formDataToBePosted, '/backcall'); // передаем поля формы на сервер
+    // Код для связи с backend:
+    // sendFormToServer(formDataToBePosted, '/backcall'); // передаем поля формы на сервер
     // Код для front-end отладки:
-    // popUpSection.classList.add('showPopUp');
-    // popUpText.innerHTML = 'Thank you, your request has been sent, our specialists will contact you within 24 hours.';
+    popUpSection.classList.add('showPopUp');
+    popUpText.textContent = 'Thank you, your request has been sent, our specialists will contact you within 24 hours.';
 }
 
 // Работа отправки формы обратной связи
@@ -184,10 +197,11 @@ function submitContactUsForm(event) {
     let formData = new FormData(contactUsForm); // создаем объект класса FormData, который черпает информацию из полей формы
     let formDataToBePosted = Object.fromEntries(formData); // преобразуем вышеописанный объект в объект "ключ-значение"
     // тем самым вышеописанная переменная хранит в себе все заполненные поля в форме
-    sendFormToServer(formDataToBePosted, '/contact'); // передаем поля формы на сервер
+    // Код для связи с backend:
+    // sendFormToServer(formDataToBePosted, '/contact'); // передаем поля формы на сервер
     // Код для front-end отладки:
-    // popUpSection.classList.add('showPopUp');
-    // popUpText.innerHTML = 'Thank you, your request has been sent, our specialists will contact you within 24 hours.';
+    popUpSection.classList.add('showPopUp');
+    popUpText.textContent = 'Thank you, your request has been sent, our specialists will contact you within 24 hours.';
 }
 
 // Отправка формы на сервер
@@ -208,12 +222,12 @@ async function sendFormToServer(formDataToBePosted, formURL) {
     // если сообщение успешно обработано сервером, т.е. мы получили ключ sent
     if (result.email === 'sent') {
         // выдаем сообщение что все хорошо
-        popUpText.innerHTML = 'Thank you, your request has been sent, our specialists will contact you within 24 hours.';
+        popUpText.textContent = 'Thank you, your request has been sent, our specialists will contact you within 24 hours.';
         // активируем popUp окно
         popUpSection.classList.add('showPopUp');
     } else { // в других случаях
         // выдаем сообщение что что-то пошло не так
-        popUpText.innerHTML = 'Something went wrong. Please, try again later';
+        popUpText.textContent = 'Something went wrong. Please, try again later';
         // активируем popUp окно
         popUpSection.classList.add('showPopUp');
     }
@@ -222,10 +236,164 @@ async function sendFormToServer(formDataToBePosted, formURL) {
 // Логика кнопки закрытия pop-up окна
 function closePopUp() {
     popUpSection.classList.remove('showPopUp');
-    popUpText.innerHTML = '';
+    popUpText.textContent = '';
 }
 
-// Обработчики событий
+// Работа смены плейсхолдера в backcall-форме и включение нужной валидации
+// запускается при загрузке страницы, а также при нажатии любой из радиокнопок
+// физический смысл: при нажатии радиокнопок Telegram (по умолчанию), WhatsApp или Phone
+// placeholder второго поля меняется на Phone и включается валидация на телефон
+// при нажатии радиокнопки Email placeholder второго поля меняется на Email и включается валидация на email
+function changePlaceholderAndLaunchValidation() {
+    // обращаемся к нажатой радиокнопке и забираем ее значение (которое передается на сервер)
+    let currentRadioButtonValue = document.querySelector('input[name="backCall"]:checked').value;
+    // если значение нажатой радиокнопки есть telegram, whatsapp или phone
+    if (currentRadioButtonValue == 'telegram' || currentRadioButtonValue == 'whatsapp' || currentRadioButtonValue == 'phone') {
+        // меняем плейсхолдер на Phone
+        phoneOrEmailInputField.placeholder = 'Phone';
+        // возвращаем стили и значения на начальные (см. описание в функции)
+        resetValidationData(phoneOrEmailInputField);
+        // убираем, что при вводе пользователем чего-либо происходит валидация email
+        phoneOrEmailInputField.removeEventListener('input', emailValidation);
+        // включаем наблюдение за вводом пользователя и при вводе проводим валидацию телефона
+        phoneOrEmailInputField.addEventListener('input', phoneValidation);
+    // если же значение нажатой радиокнопки есть email
+    } else if (currentRadioButtonValue == 'email') {
+        // меняем плейсхолдер на Email
+        phoneOrEmailInputField.placeholder = 'Email';
+        // возвращаем стили и значения на начальные (см. описание в функции)
+        resetValidationData(phoneOrEmailInputField);
+        // убираем, что при вводе пользователем чего-либо происходит валидация телефона
+        phoneOrEmailInputField.removeEventListener('input', phoneValidation);
+        // включаем наблюдение за вводом пользователя и при вводе проводим валидацию email
+        phoneOrEmailInputField.addEventListener('input', emailValidation);
+    // если же значение нажатой радиокнопки есть ни email, ни telegram, ни whatsapp и не phone
+    // (на случай чего)
+    } else {
+        // меняем плейсхолдер на Phone or Email
+        phoneOrEmailInputField.placeholder = 'Phone or email';
+        // возвращаем стили и значения на начальные (см. описание в функции)
+        resetValidationData(phoneOrEmailInputField);
+        // убираем все валидации
+        phoneOrEmailInputField.removeEventListener('input', phoneValidation);
+        phoneOrEmailInputField.removeEventListener('input', emailValidation);
+        // бросаем ошибку
+        throw new Error('Radiobutton value is not recognised');
+    }
+}
+
+// Сброс валидации
+// при смене радиокнопки нужно удалить введенные в поле данные,
+// а также убрать все, что могло появиться, если пользователь
+// ввел некорректные данные, а потом переключил валидацию с Email на Phone или наоборот
+function resetValidationData(inputElement) { // на вход функция получает элемент типа <input>
+    inputElement.value = ''; // удаляем введенные в поле пользователем значения
+    inputElement.nextElementSibling.textContent = ''; // убираем сообщение о том, что что-то введено неправильно
+    // (это элемент <p>, который идет следующим после поля)
+    inputElement.style.removeProperty('border'); // удаляем красную обводку
+    inputElement.dataset.valid = 'false'; // устанавливаем значение о валидности поля в false
+}
+
+// Сброс значений форм при обновлении или загрузке страницы
+function resetFormsOnLoad() {
+    backCallForm.reset(); // удаляем значения в форме обратного звонка
+    contactUsForm.reset(); // удаляем значения в форме обратной связи
+    scheduleCallButton.disabled = 'true'; // кнопки в обоих формах по умолчанию делаем заблокированными
+    freeConsultationButton.disabled = 'true';
+}
+
+// Валидация email
+const emailRegExp = /[A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z]{2,4}/; // регулярное выражение для почты
+function emailValidation(event) {
+    if (emailRegExp.test(event.target.value)) { // если значение в поле подходит под регулярное выражение
+        event.target.nextElementSibling.textContent = ''; // у элемента, содержащего сообщение о том, что валидация не пройдена,
+        // убираем текст, тем самым прячем элемент
+        // (предполагается, что это элемент <p>, который идет следующим за полем ввода)
+        event.target.style.removeProperty('border'); // удаляем красную обводку
+        event.target.dataset.valid = 'true'; // у поля ставим аттрибут data-valid="true"
+        activateOrDisableButton(event.target.parentElement); // проверяем, можно ли включить кнопку или нет
+        // передаем в функцию значения элемента form
+        // предполагается, что form есть "родитель" поля ввода
+    } else { // если валидация не пройдена
+        // у элемента, содержащего сообщение о том, что валидация не пройдена,
+        // присваиваем текст, тем самым показываем элемент
+        // (предполагается, что это элемент <p>, который идет следующим за полем ввода)
+        event.target.nextElementSibling.textContent = 'The e-mail address entered is invalid';
+        // добавляем красную обводку
+        event.target.style.border = '1px solid #DD1414';
+        // у поля ставим аттрибут data-valid="true"
+        event.target.dataset.valid = 'false';
+        // проверяем, надо ли выключать кнопку или она уже выключена
+        activateOrDisableButton(event.target.parentElement); // передаем в функцию значения элемента form
+        // предполагается, что form есть "родитель" поля ввода
+    }
+}
+
+// Валидация телефона
+const phoneRegExp = /(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d/i; // регулярное выражение для телефона
+function phoneValidation(event) {
+    if (phoneRegExp.test(event.target.value)) { // работа функции аналогична валидации email
+        event.target.nextElementSibling.textContent = '';
+        event.target.style.removeProperty('border');
+        event.target.dataset.valid = 'true';
+        activateOrDisableButton(event.target.parentElement);
+    } else {
+        event.target.nextElementSibling.textContent = 'The phone entered is invalid';
+        event.target.style.border = '1px solid #DD1414';
+        event.target.dataset.valid = 'false';
+        activateOrDisableButton(event.target.parentElement);
+    }
+}
+
+
+// Валидация на наличие хотя бы одного символа
+function atLeastOneSymbolValidation(event) {
+    // если значение формы не пустое
+    if (event.target.value) {
+        event.target.nextElementSibling.textContent = ''; // работа функции аналогична валидации email
+        event.target.style.removeProperty('border');
+        event.target.dataset.valid = 'true';
+        activateOrDisableButton(event.target.parentElement);
+    } else {
+        event.target.nextElementSibling.textContent = 'The name entered is empty';
+        event.target.style.border = '1px solid #DD1414';
+        event.target.dataset.valid = 'false';
+        activateOrDisableButton(event.target.parentElement);
+    }
+}
+
+// Активация или деактивации кнопки
+// функция проверяет, все ли элементы формы прошли валидацию
+// и при возможности активирует/деактивирует кнопку
+function activateOrDisableButton(formElement) { // на вход принимает элемент типа <form>
+    if (formElement.classList[0] == 'backCall__inputForm') { // если форма содержит класс backCall__inputForm
+        // то мы понимаем, что речь идет о форме обратного звонка
+        // проверяем, если оба поля прошли валидацию,
+        // то есть в поле name и в поле phoneOrEmail теги data-valid="true",
+        if (nameInputField.dataset.valid == 'true' && phoneOrEmailInputField.dataset.valid == 'true') {
+            // то активируем кнопку (то есть выключаем disabled аттрибут)
+            scheduleCallButton.disabled = false;
+        // в другом случае (т.е. хотя бы один из тегов data-valid не равен true)
+        } else {
+            // выключаем кнопку (то есть включаем disabled аттрибут)
+            scheduleCallButton.disabled = true;
+        }
+    } else if (formElement.classList[0] == 'contactUs__form') { // если форма содержит класс contactUs__form
+        // то мы понимаем, что речь идет о форме обратной связи
+        // проверяем, если оба поля со звездочкой прошли валидацию,
+        // то есть в поле firstName и в поле eMail теги data-valid="true",
+        if (firstNameInput.dataset.valid == 'true' && eMailInput.dataset.valid == 'true') {
+            // то активируем кнопку (то есть выключаем disabled аттрибут)
+            freeConsultationButton.disabled = false;
+        // в другом случае (т.е. хотя бы один из тегов data-valid не равен true)
+        } else {
+            // выключаем кнопку (то есть включаем disabled аттрибут)
+            freeConsultationButton.disabled = true;
+        }
+    }
+}
+
+// Обработчики событий и начальные значения
 viewPortfolioButton.addEventListener('mouseenter', changeArrowToStraight);
 viewPortfolioButton.addEventListener('mouseleave', changeArrowToUp);
 aboutUsButton.addEventListener('mouseenter', changeArrowToStraightAndChangeFont);
@@ -259,3 +427,11 @@ spoilerArray.forEach(element => {
 });
 backCallForm.addEventListener('submit', submitBackCallForm);
 contactUsForm.addEventListener('submit', submitContactUsForm);
+resetFormsOnLoad(); // при обновлении страницы сбрасываем значения всех форм
+changePlaceholderAndLaunchValidation(); // предостережение от того, что радиокнопка может быть не нажата
+radioButtonsArray.forEach(radioButton => {
+    radioButton.addEventListener('click', changePlaceholderAndLaunchValidation);
+});
+nameInputField.addEventListener('input', atLeastOneSymbolValidation);
+firstNameInput.addEventListener('input', atLeastOneSymbolValidation);
+eMailInput.addEventListener('input', emailValidation);
