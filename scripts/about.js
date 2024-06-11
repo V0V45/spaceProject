f9f503445e2c5aa432964e4c6051fcf4486da2bd
V0V45 <118116637+V0V45@users.noreply.github.com>
2024-06-11 22:47:31 +0300

    Added all sections untill footer

--- scripts/about.js
+++ scripts/about.js
@@ -18,3 +18,33 @@ freeConsultationHeaderButton.addEventListener('mouseleave', changeArrowToUpAndCh
 function goToCallbackForm() {
     window.location.href = '/index.html#contactUsForm';
 }
+
+/* АКТИВАЦИЯ COUNT UP */
+const observerDigits = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            // создаем объект класса CountUp (библиотека CountUp.js)
+            const count = new CountUp(
+                entry.target.id, // id HTML-элемента, который содержит число
+                (entry.target.dataset.decimals == 1) ? 0.0 : 0, // начальное значение - если десятичная точка есть, то 0.0;
+                // а если нет - то просто 0
+                entry.target.dataset.num, // конечное значение - хранится в аттрибуте data-num
+                entry.target.dataset.decimals || 0, // количество знаков после запятой - если есть аттрибут data-decimals, 
+                // то его значение и вбиваем; а если аттрибута нет, то 0
+                3, // длительность анимации - 3 секунды
+                {
+                    suffix: entry.target.dataset.suffix || '', // символ после числа - если аттрибут есть, то его и вписываем,
+                    // а если нет - то оставляем пустым
+                }
+            );
+            count.start(); // запускаем счетчик
+            observerDigits.unobserve(entry.target); // убираем слежку за объектом
+        }
+    })
+});
+const number1 = document.getElementById('num1');
+const number2 = document.getElementById('num2');
+const number3 = document.getElementById('num3');
+observerDigits.observe(number1);
+observerDigits.observe(number2);
+observerDigits.observe(number3);
